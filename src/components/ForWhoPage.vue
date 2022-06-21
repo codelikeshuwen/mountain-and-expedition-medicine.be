05fdf56a751321f890f7ffb40cd8d528433a81d0
Shuwen Apple <shuwen.w@hotmail.com>
2022-06-21 23:38:09 +1000

    Update forwho page

--- src/components/ForWhoPage.vue
+++ src/components/ForWhoPage.vue
@@ -15,7 +15,21 @@ export default {
   <div id="forwho" class="page">
     <PageHeader />
     <main>
-      <h1>For Who</h1>
+      <div class="bg">
+        <div id="forwho" class="container">
+          <div class="textbox">
+            <h5>FOR</h5>
+            <h1>MOUNTAINEERS & ROCK CLIMBERS</h1>
+            <p>
+              Altitude illness: what , prevention, treatment <br />
+              oxygen: when, how much l/min <br />
+              frostbite: prevention, managing <br />
+              nutrition: what, quality of food , quantity : calories
+            </p>
+          </div>
+          <img src="@/assets/images/mountaineers_0.jpg" alt="moutainners" />
+        </div>
+      </div>
     </main>
     <PageFooter />
   </div>
@@ -24,5 +38,24 @@ export default {
 <style>
 #forwho.page {
   background-image: url("@/assets/images/bg-fields.jpg");
+}
+
+#forwho.container {
+  width: 760px;
+  justify-content: space-between;
+}
+
+h5 {
+  font-family: "Maven Pro", sans-serif;
+  font-weight: 900;
+  font-size: 13px;
+  line-height: 24px;
+  margin: 0%;
+}
+
+#forwho.container img {
+  width: 244px;
+  object-fit: contain;
+  border: 2px, solid, white;
 }
 </style>
