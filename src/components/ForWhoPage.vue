7365190b84905e0aa5859788c3f1c3469a09e021
Shuwen Apple <shuwen.w@hotmail.com>
2022-07-06 23:55:39 +1000

    Continue addingn Dutch in ForWhoPage

--- src/components/ForWhoPage.vue
+++ src/components/ForWhoPage.vue
@@ -6,7 +6,9 @@ export default {
   name: "ForWhoPage",
   components: { PageHeader, PageFooter },
   data() {
-    return {};
+    return {
+      language: "nl",
+    };
   },
 };
 </script>
@@ -18,14 +20,26 @@ export default {
       <div class="bg">
         <div id="forwho" class="container">
           <div class="textbox">
-            <h5>FOR</h5>
-            <h1>MOUNTAINEERS & ROCK CLIMBERS</h1>
+            <h5 v-if="language === 'en'">FOR</h5>
+            <h5 v-if="language === 'nl'">VOOR</h5>
+            <h1 v-if="language === 'en'">MOUNTAINEERS & ROCK CLIMBERS</h1>
+            <h1 v-if="language === 'nl'">BERGBEKLIMMERS</h1>
+
             <hr />
-            <p>
+            <p v-if="language === 'en'">
               Altitude illness: what , prevention, treatment <br />
               oxygen: when, how much l/min <br />
               frostbite: prevention, managing <br />
               nutrition: what, quality of food , quantity : calories
+            </p>
+            <p v-if="language === 'nl'">
+              hoogteziekte: wat , hoe voorkomen, behandeling
+              <br />
+              zuurstof: wanneer , hoeveel?
+              <br />
+              vriesletsels (frostbite) : hoe voorkomen
+              <br />
+              voeding : wat, hoeveel calorieen per dag
             </p>
           </div>
           <img src="@/assets/images/mountaineers_0.jpg" alt="moutainners" />
@@ -35,10 +49,13 @@ export default {
       <div class="bg">
         <div id="forwho" class="container">
           <div class="textbox">
-            <h5>FOR</h5>
-            <h1>ATHLETES</h1>
+            <h5 v-if="language === 'en'">FOR</h5>
+            <h5 v-if="language === 'nl'">VOOR</h5>
+            <h1 v-if="language === 'en'">ATHLETES</h1>
+            <h1 v-if="language === 'nl'">ATLETEN</h1>
+
             <hr />
-            <p>
+            <p v-if="language === 'en'">
               Marathon des sables, Iron man, mountainbike trekkings
               <br />
               endurance performance
@@ -46,6 +63,15 @@ export default {
               nutrition : energy- and fluid balance
               <br />
               acclimatisation.
+            </p>
+            <p v-if="language === 'nl'">
+              Marathon des sables, Iron Man, ultraloop, mountainbike tochten
+              <br />
+              Training
+              <br />
+              voeding : energie-en vochtbalans
+              <br />
+              acclimatisatie
             </p>
           </div>
           <img src="@/assets/images/desert-bikers_0.jpg" alt="desert-bikers" />
@@ -55,10 +81,12 @@ export default {
       <div class="bg">
         <div id="forwho" class="container">
           <div class="textbox">
-            <h5>FOR</h5>
-            <h1>DESERT EXPEDITIONS</h1>
+            <h5 v-if="language === 'en'">FOR</h5>
+            <h5 v-if="language === 'nl'">VOOR</h5>
+            <h1 v-if="language === 'en'">DESERT EXPEDITIONS</h1>
+            <h1 v-if="language === 'nl'">WOESTIJNEN & OUTBACKS</h1>
             <hr />
-            <p>
+            <p v-if="language === 'en'">
               acclimatisation
               <br />
               heat related illness: what, prevention, treatment
@@ -66,6 +94,14 @@ export default {
               clothing , fluid intake
               <br />
               scorpione- and snakebites : envenomation : prevention , treatment
+            </p>
+            <p v-if="language === 'nl'">
+              acclimatisatie
+              <br />
+              hitte gerelateerde ziekten : wat , preventie , behandeling
+              <br />
+              kledij <br />
+              schorpioen- en slangenbeten : wat doen , hoe voorkomen
             </p>
           </div>
           <img src="@/assets/images/desert_0.jpg" alt="desert" />
@@ -75,10 +111,13 @@ export default {
       <div class="bg">
         <div id="forwho" class="container">
           <div class="textbox">
-            <h5>FOR</h5>
-            <h1>JUNGLE EXPEDITIONS</h1>
+            <h5 v-if="language === 'en'">FOR</h5>
+            <h5 v-if="language === 'nl'">VOOR</h5>
+            <h1 v-if="language === 'en'">JUNGLE EXPEDITIONS</h1>
+            <h1 v-if="language === 'nl'">OERWOUDEN / JUNGLES</h1>
+
             <hr />
-            <p>
+            <p v-if="language === 'en'">
               The hazards and challenges of humid tropical environments
               <br />
               tropical diseases: malaria, dengue
@@ -86,6 +125,14 @@ export default {
               insect bites , leeches
               <br />
               skin diseases and woudinfection: prevention, management clothing.
+            </p>
+            <p v-if="language === 'nl'">
+              tropische ziekten : malaria , dengue .....
+              <br />
+              insectenbeten <br />
+              huidziekten door de hoge vochtigheidsgraad
+              <br />
+              typische kledij.
             </p>
           </div>
           <img src="@/assets/images/jungle_0.jpg" alt="jungle" />
@@ -95,10 +142,13 @@ export default {
       <div class="bg">
         <div id="forwho" class="container">
           <div class="textbox">
-            <h5>FOR</h5>
-            <h1>POLE EXPEDITIONS</h1>
+            <h5 v-if="language === 'en'">FOR</h5>
+            <h5 v-if="language === 'nl'">VOOR</h5>
+            <h1 v-if="language === 'en'">POLE EXPEDITIONS</h1>
+            <h1 v-if="language === 'nl'">POOLREIZIGERS</h1>
+
             <hr />
-            <p>
+            <p v-if="language === 'en'">
               For any one challenging the Arctic North (Siberia, Alaska, Canada,
               North Pole) or the Antartic South (Antartica).
               <br />
@@ -106,6 +156,17 @@ export default {
 
               <br />
               Deshydratation: what , prevention
+            </p>
+            <p v-if="language === 'nl'">
+              Voor iedereen die zich waagt aan het Arctische Noorden (Siberië,
+              Alaska, Canada, de Noordpool, ...) of het Antarctisch Zuiden
+
+              <br />
+              Vriesletsels
+
+              <br />
+              Deshydratatie : wat , preventie; Hypothermie(onderkoeling) :
+              kledij ...
             </p>
           </div>
           <img
@@ -118,10 +179,13 @@ export default {
       <div class="bg">
         <div id="forwho" class="container">
           <div class="textbox">
-            <h5>FOR</h5>
-            <h1>CAVE & DEEP-SEA DIVERS</h1>
+            <h5 v-if="language === 'en'">FOR</h5>
+            <h5 v-if="language === 'nl'">VOOR</h5>
+            <h1 v-if="language === 'en'">CAVE & DEEP-SEA DIVERS</h1>
+            <h1 v-if="language === 'nl'">GROT- & DIEPZEEDUIKERS</h1>
+
             <hr />
-            <p>
+            <p v-if="language === 'en'">
               The risks and challenges of deep-see diving (decompression,
               preparations,, how to deal with sharks and other see creatures,
               coral cuts)
@@ -129,6 +193,15 @@ export default {
               The bends, barotrauma : prevention, managing
               <br />
               Dive tables
+            </p>
+            <p v-if="language === 'nl'">
+              De risico's en uitdagingen van diepzeeduikers
+              <br />
+              Decompressieziekte ( the bends) : wat , preventie
+              <br />
+              Contact met onderwaterflora en -fauna
+              <br />
+              Duiktabellen : wat , hoe interpreteren
             </p>
           </div>
           <img src="@/assets/images/diver_0.jpg" alt="diver" />
